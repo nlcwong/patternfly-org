6d3ca1fd992b01e3ecb020669f33e027a41d0ece
Christie Molloy <cmolloy@redhat.com>
2019-04-15 14:23:24 -0400

    update to pf4 banner

--- packages/patternfly-4/src/components/layout.js
+++ packages/patternfly-4/src/components/layout.js
@@ -124,7 +124,7 @@ class Layout extends React.Component {
           <div class="pf-l-flex pf-m-justify-content-space-between">
             <div class="pf-u-my-md">
             <span class="pf-u-ml-xl custom-text-hide">Looking for PatternFly 3? All documentation and code examples are still available.</span>
-            <a href="" style={{fontWeight: 900, textDecoration: 'none'}} class="pf-u-mx-md">Go to PatternFly 3<i class="fas fa-arrow-right pf-u-mx-sm"></i></a>
+            <a href="" style={{fontWeight: 900, textDecoration: 'none'}} class="pf-u-mx-md custom-text-link">Go to PatternFly 3<i class="fas fa-arrow-right pf-u-mx-sm"></i></a>
             </div>
             <div class="pf-u-mr-xl custom-close-button">
               <Button variant="plain" aria-label="Action">
